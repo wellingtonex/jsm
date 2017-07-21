2ed9d58fe6920297213daaaa5889e8986d6d775c
wellington.siqueira <wellingtonex@gmail.com>
2017-07-20 23:18:54 -0300

    utilizando uma textMessage

--- src/br/com/caelum/jms/ConsumidorTest.java
+++ src/br/com/caelum/jms/ConsumidorTest.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class ConsumidorTest {
@@ -30,7 +32,13 @@ public class ConsumidorTest {
 				
 				@Override
 				public void onMessage(Message message) {
-					System.out.println(message);
+					TextMessage textMessage = (TextMessage) message;
+					try {
+						System.out.println(textMessage.getText());
+					} catch (JMSException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					
 				}
 			});
